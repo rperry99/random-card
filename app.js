3691ae44689d4bac03e08e6c49dfacf796a0ddeb
Russ Perry <rperry1886@gmail.com>
2019-02-01 18:47:44 -0500

    Start planning out the logic for what your hand is.

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-//Variable to hold your card
-let yourCard = "";
-
 //Arrays for Cards
 let rank = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"];
-let suit = ["♥", "♦", "♣", "♠"];
+let suit = ["Hearts", "Diamonds", "Clubs", "Spades"];
 
 //Array to hold deck of cards
 let deck = [];
@@ -14,18 +11,46 @@ let cardText = document.getElementById("card");
 //Populate the deck of cards
 for (let i = 0; i < suit.length; i++) {
   for (let j = 0; j < rank.length; j++) {
-    deck.push(rank[j] + " " + suit[i]);
+    deck.push(rank[j] + " of " + suit[i]);
   }
 }
 
 //Push the button, get a card
 function getCard() {
-  let x = Math.floor(Math.random() * 52);
-  document.getElementById("yourCard").style.display = "block";
+  let hand = [];
+  let handRanks = [];
+  let handSuits = [];
+  let handText;
   shuffle(deck);
-  cardText.innerHTML = `Your hand is: ${deck[0]}, ${deck[1]}, ${deck[2]}, ${
-    deck[3]
-  }, and ${deck[4]}`;
+  for (let i = 0; i < 5; i++) {
+    hand.push(deck[i]);
+    if (deck[i][0] == 1) {
+      handRanks.push("10");
+    } else {
+      handRanks.push(deck[i][0]);
+    }
+    handSuits.push(deck[i][5]);
+  }
+  whatsYourHand(handRanks, handSuits);
+  handText = hand.join(", ");
+  cardText.style.display = "block";
+  cardText.innerHTML = `Your hand is: ${handText}`;
+}
+
+function whatsYourHand(rank, suits) {
+  rank.sort();
+  suits.sort();
+  console.log(rank, suit);
+  // 1. Royal Flush
+  // 2. Straight Flush
+  // 3. 4 Of A Kind
+  // 4. Full House
+  // 5. Flush
+  // 6. Straight
+  // 7. 3 of a kind
+  // 8. 2 Pair
+  // 9. Pair
+  // 10. High Card
 }
 
 //function to shuffle the cards
